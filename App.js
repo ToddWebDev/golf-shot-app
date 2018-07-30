1854db61fb3a505ec2abddf202fae9730a33eddf
ToddWebDev <webdevtodd@gmail.com>
2018-07-30 07:04:46 -0500

    Integrate Firebase

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/header/Header';
-import Login from './src/components/login/Login';
 import CourseList from './src/components/course/CourseList';
 import ScoreList from './src/components/score/ScoreList';
 import { Provider } from 'react-redux';
@@ -13,16 +12,12 @@ export default class App extends React.Component {
   render() {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
     return (
-      <View>
-        <Header title="Golf Shot" />
-        <Login />      
-      </View>
-      // <Provider store={store}>
-      //   <View>
-      //     <Header title="Golf Shot" />
-      //     <CourseList />
-      //   </View>
-      // </Provider>
+      <Provider store={store}>
+        <View>
+          <Header title="Golf Shot" />
+          <CourseList />
+        </View>
+      </Provider>
     );
   }
 }
